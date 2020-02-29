73be5437e73b560273f57a12122f4442d9d521c3
misostack <misostack@gmail.com>
2020-02-29 22:23:55 +0700

    standard output

--- headfirst/cs1/cs1.py
+++ headfirst/cs1/cs1.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import constant
 print("In the first lession of python tutorial. We'll learn about: \n" \
         "1. Variables\n" \
         "2. Datatypes\n" \
         "3. Operaters\n" \
-        "4. Decision Making\n" \
-        "5. Loops\n" \
 )
 
 print("1.Variables\n")
@@ -15,14 +13,18 @@ lastName = "Nguyen" # string
 age = 32 # integer
 averageSalary = float(26000000) / float(constant.USD_TO_VND) # float
 
-print( "Fullname: " + firstName + " " + lastName + \
-       "Age: " + str(age) + \
+print( "Fullname: " + firstName + " " + lastName + '\n' + \
+       "Age: " + str(age) + '\n' + \
        "Average Salary: " + str(averageSalary) + "\n" )
 
 print( "Fullname: %s %s\n" % (firstName, lastName) + \
         "Age: %d\n" % age + \
         "Average Salary: %0.2f USD/month" % round(averageSalary, 2))
-
-print( "Fullname: {firstName} {lastName}\n" + \
+print("-"*100)
+print((
+       "Fullname: {firstName} {lastName}\n" + \
        "Age: {age}\n" + \
-       "Average Salary: {averageSalary}\n".format(firstName=firstName, lastName=lastName, age=age, averageSalary=averageSalary))
+       "Average Salary: {averageSalary}\n") \
+       .format(firstName=firstName, lastName=lastName, age=age, averageSalary=averageSalary) )
+
+print("Fullname: {}".format(firstName + lastName))
