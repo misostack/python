0b1ebad9bd59de4459acef54f6376b9018327635
misostack <misostack.com@gmail.com>
2020-03-22 14:26:18 +0700

    Read args from input

--- headfirst/cs1/input.py
+++ headfirst/cs1/input.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
-import sys, getopt
+# import sys, getopt
 
-print("1.Input\n")
+# print("1.Input\n")
 
-# print(sys.argv)
-try:
-  opts, args = getopt.getopt(sys.argv[1:],"i:o",["input=", "output="])
-  print(opts)
-except getopt.GetoptError:
-  sys.exit(2)
+# try:
+#   opts, args = getopt.getopt(sys.argv[1:],"i:o",["input=", "output="])
+#   print(opts)
+# except getopt.GetoptError:
+#   sys.exit(2)
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                   help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                   const=sum, default=max,
+                   help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
